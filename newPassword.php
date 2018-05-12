ffc33204782fba51b30fe8a38a15a0430474de81
Tonke Bult <tonksol@gmail.com>
2018-05-12 16:21:01 +0200

    finally, fixed the output for the columns in the table for the CPT !

--- newPassword.php
+++ newPassword.php
@@ -9,6 +9,7 @@
 * 
 */
 
+// use return error_log() for debugging
 if ( ! defined( 'ABSPATH' ) ) exit; // Security: Exit if accessed directly
 
 
@@ -17,16 +18,18 @@ class NewPasswordPlugin {
         // hook, array(class name, method name)
         add_action('wp_enqueue_scripts', array($this, 'loadResources'));
         add_action('admin_menu', array($this, 'addPage'));
-       // add_action('wp_head', 'addInHeadForLogin');
         add_filter('the_content', array($this, 'addLoginContent'));
         add_action('admin_post_new_password_hook', array($this, 'formValidation'));
         
         // hooks for admin
         add_action('init', array($this,'add_newclients_to_admin')); // add a menu in the WordPress admin panal
         add_filter('manage_cpt_newclients_posts_columns' , array($this,'add_columns_to_newclients'));
+        add_action( 'manage_cpt_newclients_posts_custom_column' , array($this, 'read_columns'), 10, 2);
         add_action('add_meta_boxes', array($this, 'add_metaboxes_newclients'));
+        add_action('save_post', array($this, 'save_newclient_meta_data'));
     }
 
+   
     function addInHeadForLogin() {
         return 
             '<link href="//cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css" rel="stylesheet" id="bootstrap-css">
@@ -136,27 +139,59 @@ class NewPasswordPlugin {
         );
     }
 
-    public function remove_metaboxes() {
-        remove_meta_box( $id, 'post', 'advanced' );
-    }
+
+    
+
+function read_columns($column, $post_id) {
+    error_log("Reading column $column for post $post_id");
+	switch ( $column ) {
+	     case 'firstname':
+		 	echo get_post_meta($post_id, 'firstname', true);
+             break;
+		case 'prefix':
+			echo get_post_meta( $post_id, 'prefix', true ); 
+            break;
+        case 'lastname':
+			echo get_post_meta( $post_id, 'lastname', true ); 
+			break;
+	}
+}
+
+
+// add_action( 'manage_posts_custom_column' , 'custom_columns', 10, 2 );
+// 
+// function custom_columns( $column, $post_id ) {
+// 	switch ( $column ) {
+// 		case 'book_author':
+// 			$terms = get_the_term_list( $post_id, 'book_author', '', ',', '' );
+// 			if ( is_string( $terms ) ) {
+// 				echo $terms;
+// 			} else {
+// 				_e( 'Unable to get author(s)', 'your_text_domain' );
+// 			}
+// 			break;
+// 
+// 		case 'publisher':
+// 			echo get_post_meta( $post_id, 'publisher', true ); 
+// 			break;
+// 	}
+// }
 
     public function add_metaboxes_newclients() {
         $screens = ['post', 'cpt_newclients'];
         foreach ($screens as $screen) {
             add_meta_box(
                 'newclients_box_id',                    // Unique ID
-                'Add Client Info',                // Box title
+                'Add Client Info',                      // Box title
                 array($this,'newclients_box_html'),     // Content callback, must be of type callable
                 $screen                                 // Post type
             );
         }
-        
-      }
+    }
 
-        /* Display the post meta box. */
+    // Display the post meta box.
     public function newclients_box_html() {
         ?>
-        <form>
         <input type="text" name="firstname" placeholder="First Name"> 
         <br><br>
         <input type="text" name="prefix" placeholder="Prefix"> 
@@ -169,9 +204,38 @@ class NewPasswordPlugin {
         <br><br>
         <?php
     }
+
+        /**
+     * Save post metadata when a post is saved.
+     *
+     * @param int $post_id The post ID.
+     * @param post $post The post object.
+     * @param bool $update Whether this is an existing post being updated or not.
+     */
+    public function save_newclient_meta_data($post_id) {
+        /*
+        * In production code, $slug should be set only once in the plugin,
+        * preferably as a class property, rather than in each function that needs it.
+        */
+        $post_type = get_post_type($post_id);
+
+        // If this isn't a 'book' post, don't update it.
+        if ( "cpt_newclients" != $post_type ) return;
+        
+        // - Update the post's metadata.
+        if ( isset( $_POST['firstname'] ) ) {
+            update_post_meta( $post_id, 'firstname', sanitize_text_field( $_POST['firstname'] ) );
+            error_log("hij komt in de isset(firstname): " . $_POST['firstname']);
+        }
+
+        if ( isset( $_POST['prefix'] ) ) {
+            update_post_meta( $post_id, 'prefix', sanitize_text_field( $_POST['prefix'] ) );
+            error_log("hij komt in de isset(prefix)");
+        }
+
     
-
-
+    }
+    // add_action( 'save_post', 'save_book_meta', 10, 3 );
 
 
 
